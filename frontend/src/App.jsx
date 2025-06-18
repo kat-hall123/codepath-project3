9fe5234d76bd8938dd28ff4cf6d5d6ae7e21d68d
Katherine Hall <khall206@meta.com>
2025-06-18 15:00:29 -0700

    set up react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router'
 import './css/App.css'
 
 import DashboardPage from './DashboardPage';
+import BoardDetailPage from './BoardDetailPage';
 
 function App() {
   return (
-    <>
-      <DashboardPage />
-    </>
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/boards/:boardId" element={<BoardDetailPage />} />
+    </Routes>
   )
 }
 
