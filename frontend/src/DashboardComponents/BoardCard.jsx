72f3f13adce97810db2dce72aa09fe081509715f
Katherine Hall <khall206@meta.com>
2025-06-18 16:55:21 -0700

    loaded database's boards onto the dashboard

--- frontend/src/DashboardComponents/BoardCard.jsx
+++ frontend/src/DashboardComponents/BoardCard.jsx
@@ -1,11 +1,19 @@
 import '../css/BoardCard.css';
 
-const BoardCard = () => {
+import { Link } from 'react-router';
+
+const BoardCard = ({ board }) => {
     return (
         <div className="board-card">
-            <p>Board Card</p>
+            <Link to={`/boards/${board.id}`} className="board-card-link">
+                <img src={board.imageUrl} alt={board.title} className="board-image" />
+                <div className="board-info">
+                    <h3>{board.title}</h3>
+                    <p>{board.category}</p>
+                    {board.author && <p>By {board.author}</p>}
+                </div>
+            </Link>
         </div>
-
     )
 }
 
